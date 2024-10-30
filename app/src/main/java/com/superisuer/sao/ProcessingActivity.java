5ca2ac313b032ac7d59c442a9b2ed263dfef45a9
Den <superisuer@proton.me>
2024-10-30 17:36:46 +0300

    added themed icons

--- app/src/main/java/com/superisuer/sao/ProcessingActivity.java
+++ app/src/main/java/com/superisuer/sao/ProcessingActivity.java
@@ -337,7 +337,7 @@ public class ProcessingActivity extends Activity {
 								runOnUiThread(new Runnable() {
 									@Override
 									public void run() {
-										textview1.setText(textview1.getText().toString().concat("\n! Make sure you have access to build.prop.\n> Communication has been accelerated.\n> Improved performance.\n"));
+										textview1.setText(textview1.getText().toString().concat("> Waiting for the system to respond.\n> Build.prop tweaks:\n> Communication has been accelerated.\n> Improved performance."));
 										try { 
 											Runtime.getRuntime().exec("su -c setprop ro.HOME_APP_ADJ 1");
 											Runtime.getRuntime().exec("su -c setprop ro.media.enc.jpeg.quality 100");
@@ -525,55 +525,4 @@ public class ProcessingActivity extends Activity {
 		textview2.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/light.ttf"), 0);
 	}
 	
-	
-	@Deprecated
-	public void showMessage(String _s) {
-		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
-	}
-	
-	@Deprecated
-	public int getLocationX(View _v) {
-		int _location[] = new int[2];
-		_v.getLocationInWindow(_location);
-		return _location[0];
-	}
-	
-	@Deprecated
-	public int getLocationY(View _v) {
-		int _location[] = new int[2];
-		_v.getLocationInWindow(_location);
-		return _location[1];
-	}
-	
-	@Deprecated
-	public int getRandom(int _min, int _max) {
-		Random random = new Random();
-		return random.nextInt(_max - _min + 1) + _min;
-	}
-	
-	@Deprecated
-	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
-		ArrayList<Double> _result = new ArrayList<Double>();
-		SparseBooleanArray _arr = _list.getCheckedItemPositions();
-		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
-			if (_arr.valueAt(_iIdx))
-			_result.add((double)_arr.keyAt(_iIdx));
-		}
-		return _result;
-	}
-	
-	@Deprecated
-	public float getDip(int _input) {
-		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
-	}
-	
-	@Deprecated
-	public int getDisplayWidthPixels() {
-		return getResources().getDisplayMetrics().widthPixels;
-	}
-	
-	@Deprecated
-	public int getDisplayHeightPixels() {
-		return getResources().getDisplayMetrics().heightPixels;
-	}
 }
